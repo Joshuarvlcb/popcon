008b8f604e27feb82237e2f8a02c4823a563eb34
josh <joshuarvlcb@gmail.com>
2021-07-13 21:41:29 -0700

    i am going to use the movie db api

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { FiArrowUpCircle } from "react-icons/fi";
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__image"></div>
-      <div className="footer__copy"></div>
+      <div className="footer__copy">
+        © 2021 Popcon. All Rights Reserved to Joshua Ruvalcaba.
+        <FiArrowUpCircle className="up" />
+      </div>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-
+import { FiHeart } from "react-icons/fi";
+import { BsCalendar } from "react-icons/bs";
+import { BiMovie } from "react-icons/bi";
+import { FiSettings } from "react-icons/fi";
+import { BiLogOut } from "react-icons/bi";
 function Nav() {
   return (
     <div className="nav">
       <div className="menu-section">
         <div className="menu">Menu</div>
         <ul className="menu-nav">
-          <div className="menu-nav__link">new releases</div>
-          <div className="menu-nav__link">coming soon</div>
-          <div className="menu-nav__link">genres</div>
-          <div className="menu-nav__link">watchlist</div>
+          <div className="menu-nav__link">
+            <BiMovie className="menu__icon" />
+            new releases
+          </div>
+          <div className="menu-nav__link">
+            <BsCalendar className="menu__icon soon" />
+            coming soon
+          </div>
+          <div className="menu-nav__link">
+            <FiHeart className="menu__icon heart" />
+            watchlist
+          </div>
         </ul>
       </div>
       <div className="general-section">
         <div className="general">General</div>
         <ul className="general-nav">
-          <div className="general-nav__link">Settings</div>
-          <div className="general-nav__link">Log out</div>
+          <div className="general-nav__link">
+            <FiSettings className="general__icon setting" />
+            Settings
+          </div>
+          <div className="general-nav__link">
+            <BiLogOut className="general__icon" />
+            Log out
+          </div>
         </ul>
       </div>
-      <div className="challenges"></div>
+      <div className="challenges-container">
+        <div className="challenges">
+          <div className="challenges__titles">
+            <h5>Popcorn Addict</h5>
+            <p>4/6h viewing time</p>
+          </div>
+          <button className="challenges__btn">view challenges</button>
+        </div>
+      </div>
     </div>
   );
 }
